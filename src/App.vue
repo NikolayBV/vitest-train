07307e619efe6b3fec249746730aa420e0d165a2
Nikolai Babkin <babkinnick95@gmail.com>
2024-01-15 17:32:06 +0600

    fix auth

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,50 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import { useAuthStore } from '@/store/auth/AuthStore'
-import { storeToRefs } from 'pinia'
+<script lang="ts">
 import Router from '@/router'
+import { RouterLink, RouterView } from 'vue-router'
+import { defineComponent, ref, watchEffect } from 'vue'
 
-const store = useAuthStore()
-const router = Router
-const { isAuth } = storeToRefs(store)
-if (!isAuth.value) {
-  router.push('/login')
-}
+export default defineComponent({
+  name: 'App',
+  props: {},
+  setup() {
+    const router = Router
+    const token = ref()
+    watchEffect(() => {
+      token.value = localStorage.getItem('token')
+      if (!token.value) {
+        router.push('/login')
+      }
+    })
+    return { router, token, RouterLink, RouterView }
+  }
+})
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav v-if="isAuth" class="nav-wrapper">
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/posts">Posts</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-      <nav v-else class="nav-wrapper">Login</nav>
-    </div>
-  </header>
-  <RouterView />
+  <div class="app-wrapper">
+    <header>
+      <div class="header-wrapper">
+        <nav v-if="token" class="nav-wrapper">
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/posts">Posts</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+        <nav v-else class="nav-wrapper">Login</nav>
+      </div>
+    </header>
+    <RouterView class="main" />
+    <footer>Vue Test app</footer>
+  </div>
 </template>
 
 <style scoped lang="scss">
-.wrapper {
+.app-wrapper {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.header-wrapper {
   padding: 20px;
   background-color: bisque;
   border-bottom: 1px solid;
@@ -37,5 +54,8 @@ if (!isAuth.value) {
   align-items: center;
   justify-content: center;
   gap: 20px;
+}
+.footer {
+  align-self: end;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './styles/main.css'
+import './styles/base.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
