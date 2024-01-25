a71f58428e32b45778dd0ff923c68caff8e3c8e2
Nikolai Babkin <babkinnick95@gmail.com>
2024-01-25 23:53:40 +0600

    error with mock

--- src/components/CreateNoteCars/createNoteCard.spec.ts
+++ src/components/CreateNoteCars/createNoteCard.spec.ts
@@ -1,8 +1,24 @@
-import { describe, it } from 'vitest'
+import { beforeEach, describe, it, vi } from 'vitest'
 import { mount } from '@vue/test-utils'
 import CreateNoteCard from '@/components/CreateNoteCars/CreateNoteCard.vue'
-import { testNote } from '@/utils/mocks'
 
 describe('createNoteCard', () => {
-  it('createNoteCard should renders', () => {})
+  beforeEach(() => {
+    vi.mock('vue-hooks-form', async (importOriginal) => {
+      const mod = await importOriginal<typeof import('vue-hooks-form')>()
+      const useForm = vi.fn(() => {
+        const useField = vi.fn(() => ({
+          ref: vi.fn()
+        }))
+        return useField
+      })
+      return {
+        ...mod,
+        useForm
+      }
+    })
+  })
+  it('createNoteCard should render', () => {
+    mount(CreateNoteCard)
+  })
 })
